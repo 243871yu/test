a65fe6fb3c306fb1092c29ae607cf3557fbdaa1f
baby <2438711745@qq.com>
2024-02-25 16:54:11 +0800

    测试仓库

---
+++ 0225.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+/*
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	//a = 2,b = 3;
+	printf("%d",a > b ? a : b);
+	return 0;
+}
+
+
+int main()
+{
+	int a = 5;
+	
+	a += 2;
+	a = a + 2; 
+	
+	printf("%d", a);
+	return 0;
+}
+*/
+
+int main()
+	
+	{int day;
+	scanf("%d",&day);
+	
+	printf("%d天是%d周零%d天\n", day, day / 7, day % 7 );
+	return 0;
+}
+/*
+int main()
+{
+	long long c = 299792458;
+	long long t = 365 * 86400;
+	
+	printf("%太阳到地球的距离为%lld米\n", c * t);
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
